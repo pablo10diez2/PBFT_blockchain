fcbe6caa22633b08ce6c7aee121fa4720cf6f73d
pablo10diez2 <pablo.diez@opendeusto.es>
2026-04-26 19:47:12 +0200

    Thread manager for launching threads

--- Src/app.cpp
+++ Src/app.cpp
@@ -1,27 +1,52 @@
 #include "includes/app.h"
-#include "includes/client.h"
 #include "includes/request.h"
-
-#include <thread>
+#include "includes/node.h"
+#include "includes/client.h"
 
 void init_app(){
-    std::vector< std::shared_ptr<Node> > nodes;
+    std::vector<std::shared_ptr<Node>> nodes;
     init_nodes(&nodes);
 
     Client client{};
     Request request1{true, 1};
-
-    std::thread client_read_thread(&Client::read_buffer_continuous, &client);
-
+    
+    std::thread launch_threads(thread_manager, &nodes, &client);
+    
     nodes[0].get()->send_to_client(&client, &request1);
 
-    client_read_thread.join();
+    launch_threads.join(); 
 }
 
-void init_nodes(std::vector< std::shared_ptr<Node> >* nodes){
+void init_nodes(std::vector<std::shared_ptr<Node>>* nodes){
     for(int i = 0; i < 10; ++i){
         std::shared_ptr<Node> ptr = std::make_shared<Node>();
         nodes->push_back(ptr);
     }
 }
 
+void thread_manager(std::vector<std::shared_ptr<Node>>* nodes, Client* client){
+    std::vector<std::thread> nodes_read_threads;
+
+    std::thread client_read_thread = start_client_read_thread(client);
+    start_nodes_read_threads(&nodes_read_threads, nodes);
+
+    client_read_thread.join();
+
+    for(auto& n : nodes_read_threads){
+        n.join();
+    }
+}
+
+std::thread start_client_read_thread(Client* client){
+    std::thread t(&Client::read_buffer_continuous, client);
+
+    return std::move(t);
+}
+
+void start_nodes_read_threads(std::vector<std::thread>* nodes_read_threads, std::vector<std::shared_ptr<Node>>* nodes){
+    for(const auto& node : *nodes){
+        std::thread t(&Node::read_buffer_continuous, node);
+        
+        nodes_read_threads->push_back( std::move(t) );
+    }
+}
--- Src/includes/app.h
+++ Src/includes/app.h
@@ -3,10 +3,15 @@
 
 #include <vector>
 #include <memory>
+#include <thread>
 
-#include "node.h"
+class Node;
+class Client;
 
 void init_app();
 void init_nodes(std::vector< std::shared_ptr<Node> >* nodes);
+std::thread start_client_read_thread(Client* client);
+void thread_manager(std::vector<std::shared_ptr<Node>>* nodes, Client* client);
+void start_nodes_read_threads(std::vector<std::thread>* nodes_read_threads, std::vector<std::shared_ptr<Node>>* nodes);
 
 #endif
